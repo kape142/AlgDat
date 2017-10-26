5550c7f22c5976c8618aa638c19175d46354cf78
[kape142] <[kape142@hotmail.com]>
2017-10-26 13:08:11 +0200

    øving 10

--- src/Øving10.java
+++ src/Øving10.java
@@ -3,16 +3,30 @@
  */
 public class Øving10 {
     public static void main(String[] args) {
-        String[] strings = {"Dette er en vanlig tekst","Dette er en tekst med 1 tall","1234","14/05/2007",
-                "barebokstaver","123/22/2888","29/02","92/39/9999","123456789","1234567890","1111","10"};
-        String[] regex = {".*\\d.*","(0?[1-9]|[12][0-9]|3[01])[/](0?[1-9]|1[012])[\\/\\-]\\d{4}",".{10}.*",
-                ".*[^a-zA-Z].*","(1(1*))|((1(0*))|((1*)1))"};
-        String test = "(http(s)?:\\/\\/)?(www\\.)?[A-Za-z0-9]+(\\.[A-Za-z0-9]+)+(\\/.*)*";
+        String[] strings = {
+                "Dette er en vanlig tekst",
+                "Dette er en tekst med 1 tall",
+                "1234","14/05/2007",
+                "barebokstaver",
+                "001/03/2002",
+                "29/02",
+                "92/39/9999",
+                "123456789",
+                "1234567890",
+                "1111",
+                "10"
+        };
+        String[] regex = {
+                ".*\\d.*", //streng inneholder tall
+                "(0?[1-9]|[12][0-9]|3[01])[/](0?[1-9]|1[012])[\\/\\-]\\d{4}", //streng er dato på form dd/mm/yyyy
+                ".{10}.*", //streng har minst 10 tegn
+                ".*[^a-zA-Z].*" // streng inneholder andre tegn enn bokstaver
+        };
         for (int i = 0; i < regex.length; i++) {
             System.out.println((i+1)+". regex");
             for (String s : strings) {
                 if (s.matches(regex[i])) {
-                    System.out.println(s);
+                    System.out.println("\t"+s);
                 }
             }
             System.out.println();
@@ -31,4 +45,4 @@ public class Øving10 {
 //(rs)* ((rs)*)* er like. den siste er bare 0 til mange av den første (som allerede er null til mange
 
 
-
+//nettside test "(http(s)?:\\/\\/)?(www\\.)?[A-Za-z0-9]+(\\.[A-Za-z0-9]+)+(\\/.*)*"
